3efd9d4f57e32a904b03cb7d1c02ee5b597a6af7
ziarulislamcse194 <ziarulislamcse194@gmail.com>
2019-07-23 00:24:11 +0600

    firstClass

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name");
+            string name = Console.ReadLine();
+            
+
+            Console.WriteLine("Please Enter a Address");
+            string address = Console.ReadLine();
+            
+
+            Console.WriteLine("Please Enter Contact Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Please Enter a Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Please Enter Date of Birth");
+            string dateOfBitrth = Console.ReadLine();
+
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------");
+
+            Console.WriteLine("Name is: [" + name + " ]");
+            Console.WriteLine("Address is: [" + address + "]");
+            Console.WriteLine("Contact is: [" + number + "]");
+            Console.WriteLine("Age is: [" + age + "]");
+            Console.WriteLine("Date of Birth is: [" + dateOfBitrth + "]");
+            Console.ReadKey();
+        }
+    }
+}
